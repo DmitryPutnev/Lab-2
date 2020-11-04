3cd6f81e8413c6e6b05091383c5cdf7d2f870d38
DmitryPutnev <71452259+DmitryPutnev@users.noreply.github.com>
2020-11-04 23:16:47 +0300

    Delete function.c

--- function.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int glas(char a)
-{
-	if ((a == 'A') || (a == 'a') || (a == 'U') || (a == 'u') || (a == 'E')
-		|| (a == 'e') || (a == 'O') || (a == 'o') || (a == 'I') || (a == 'i'))
-		return 1;
-	else
-		return 0;
-}
